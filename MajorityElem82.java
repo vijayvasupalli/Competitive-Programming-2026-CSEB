bdaf98845adcc9274263df375d6319df03a86cef
vijay.vasupalli_ <vijaymanoharvasupalli07@gmail.com>
2026-07-22 12:28:39 +0530

    Add Majority Element Finder in MajorityElem82.java
    Implemented a solution to find the majority element in an array.

---
+++ MajorityElem82.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int ans = -1;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            if (count > n / 2) {
+                ans = arr[i];
+                break;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
